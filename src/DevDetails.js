0ffdeab8b07f0bc8b9d39bef755bcc53df433a81
Rose <hmoore96@outlook.com>
2021-05-01 16:45:18 +0100

    added recommended attribute to link

--- src/DevDetails.js
+++ src/DevDetails.js
@@ -3,7 +3,11 @@ import React from "react";
 export default function DevDetails() {
   return (
     <span>
-      <a href="https://github.com/HR0se/weather-react-app" target="_blank">
+      <a
+        href="https://github.com/HR0se/weather-react-app"
+        target="_blank"
+        rel="noreferrer"
+      >
         Open-source code on GitHub
       </a>{" "}
       by Harriet Rose
