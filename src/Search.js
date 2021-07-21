cdb1211544a367548f06f9f038153ded247750be
Rose <hmoore96@outlook.com>
2021-07-21 15:06:00 +0100

    integrated 3 search components into 1

--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,32 @@
 import React from "react";
-import SearchInput from "./SearchInput";
-import SearchButton from "./SearchButton";
-import LocationButton from "./LocationButton";
 
 export default function Search() {
   return (
     <div className="Search">
       <form id="search-engine">
         <div className="row">
-          <SearchInput />
-          <SearchButton />
-          <LocationButton />
+          <div className="searchInput col-md-5">
+            <input
+              type="text"
+              placeholder="Please enter your city"
+              className="search-bar-item search-bar"
+              id="search-bar"
+              autocomplete="off"
+              autofocus="on"
+            />
+          </div>
+          <div className="SearchButton col-md-3">
+            <input
+              type="submit"
+              value="Search"
+              className="search-button search-bar-item"
+            />
+          </div>
+          <div className="LocationButton col-md-4">
+            <button className="search-button search-bar-item">
+              Current Location
+            </button>
+          </div>
         </div>
       </form>
     </div>
